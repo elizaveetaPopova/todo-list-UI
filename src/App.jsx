a7b849ac3a45d7a17104e62ea8055847866efe15
elibychkova <eli.bychkova@gmail.com>
2023-11-16 11:46:57 +0300

    added logic for popup, button for deletion tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
 import Card from "@mui/material/Card";
-import Modal from "@mui/material/Modal";
+// import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
+import Popup from "./components/Popup";
 
-import FormComponent from "./components/FormComponent";
+import CheckboxList from "./components/CheckboxList";
 
 import "./styles/app.css";
+import { useState } from "react";
 
 const App = () => {
+  const [isPopupOpen, setPopupOpen] = useState(false);
+  const handleOpen = () => setPopupOpen(true);
+  const handleClose = () => setPopupOpen(false);
+
+  console.log("isPopupOpen :>> ", isPopupOpen);
   return (
     <div className="app">
       <Card className="tasksCard">
+        <Popup onClose={handleClose} isPopupOpen={isPopupOpen} />
         <Button
           endIcon={<AddIcon fontSize="small" />}
           variant="contained"
           color="success"
+          onClick={handleOpen}
         >
           Добавить новую задачу
         </Button>
-        <FormComponent />
+        <CheckboxList />
+        <Button variant="contained" color="error">
+          Удалить выполненные задачи
+        </Button>
       </Card>
     </div>
   );
